50e277ec2809c213a35356c5d18b456a5e9ba559
Njeri Kieha <sylvia.kieha@andela.com>
2018-01-18 02:19:31 +0300

    Adding todos

---
+++ todoReducer.js
@@ -0,0 +1,40 @@
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ];
+    default:
+      return state;
+  }
+};
+
+const testAddTodo = () => {
+  const stateBefore = [];
+  const action = {
+    type: "ADD_TODO",
+    id: 0,
+    text: "Learn redux"
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: "Learn redux",
+      completed: false
+    }
+  ];
+  
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+  
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+}
+
+testAddTodo();
